6cd368c6640e3ede1ed9c2d7d8b6dbb3183bc3d7
Samuel Leal <samuelmarquessousaleal@gmail.com>
2022-11-12 17:52:41 -0300

    Atualizando a validacao backend #53

---
+++ src/backend/middlewares/globalMiddleware.js
@@ -0,0 +1,34 @@
+const isNullOrUndefined = (campo) => {
+  if (campo === null || campo === undefined) {
+      return true;
+  }
+  return false;
+}
+
+const isStringEmpty = (varString) => {
+  if (varString === " ") {
+      return true;
+  }
+  return false;
+}
+
+const isNumberValid = (varNumber) => {
+  if (varNumber < 0) {
+      return false
+  }
+  return true;
+}
+
+const isSeniorityValid = (senioridade) => {
+  if(senioridade != 'estagiario' && senioridade != 'junior' && senioridade != 'pleno' && senioridade != 'senior'){
+      return false;
+  }
+  return true;
+}
+
+module.exports = {
+  isNullOrUndefined,
+  isStringEmpty,
+  isNumberValid,
+  isSeniorityValid
+};
--- src/backend/middlewares/vagasMiddleware.js
+++ src/backend/middlewares/vagasMiddleware.js
@@ -1,3 +1,5 @@
+const { isNullOrUndefined, isStringEmpty, isSeniorityValid, isNumberValid } = require('globalMiddleware');
+
 const validateBody = (req, res, next) => {
     const { body } = req;
 
@@ -17,34 +19,6 @@ const validateBody = (req, res, next) => {
 
     next();
 };
-
-const isNullOrUndefined = (campo) => {
-    if (campo === null || campo === undefined) {
-        return true;
-    }
-    return false;
-}
-
-const isStringEmpty = (varString) => {
-    if (varString === " ") {
-        return true;
-    }
-    return false;
-}
-
-const isNumberValid = (varNumber) => {
-    if (varNumber < 0) {
-        return false
-    }
-    return true;
-}
-
-const isSeniorityValid = (senioridade) => {
-    if(senioridade != 'estagiario' && senioridade != 'junior' && senioridade != 'pleno' && senioridade != 'senior'){
-        return false;
-    }
-    return true;
-}
 
 module.exports = {
     validateBody
