410d3dea75e90464709f9130e265e8f1ad5bebf5
CamillaCruz <camillaregina70@gmail.com>
2022-11-15 22:55:20 -0300

    Integração entre fronted e backend da página Listagem de Vagas #55

--- src/frontend/scripts/listaDeVagasRH.js
+++ src/frontend/scripts/listaDeVagasRH.js
@@ -1,14 +1,64 @@
 const uri = "http://localhost:8081/vagas"
+let textoHTML = '';
 
 async function getItems() {
   const headers = new Headers();
   headers.append('Content-Type', 'application/json');
   headers.append('Accept', 'application/json');
 
- const data = await fetch(uri, {
+  const data = await fetch(uri, {
     method: "GET",
     headers: headers,
-  }).then((response) => {return response.json()});
+  }).then((response) => { return response.json() });
 
   console.log(data);
+
+  Array.prototype.forEach.call(data, function (x, y) {
+    console.log("nomeDoCampo: " + y + " valor: " + x.descricao);
+    const divVaga = document.getElementById("container_vagas");
+    // if para evitar repetição de linha undefined (RTA)
+    if (x != undefined) {
+      divVaga.innerHTML = contruirCorpoModal(x);
+    }
+  });
+
+  function contruirCorpoModal(data) {
+    let codigoStatus = data.codigoStatus;
+    let dscStatus = data.dscStatus;
+    let codigoVaga = data.codigoVaga;
+    let descricao = data.descricao;
+    let qtdVagas = data.qtdVagas;
+    let requisitos = data.requisitos;
+    let senioridade = data.senioridade;
+    let tempoExperienciaVaga = data.tempoExperienciaVaga;
+    let tituloVaga = data.tituloVaga;
+    let localModalidade = data.localModalidade;
+
+    textoHTML += `
+      <div class="new-vaga">
+        <div class="vaga">
+          <div class="vaga-info">
+            <div id="nome-vaga">${tituloVaga}</div>
+            <div id="local-vaga">${localModalidade}</div>
+            <div id="status-vaga">${dscStatus}</div>
+
+            <div id="botoes">
+              <div class="button_container">
+                <button class="button" id="button-trash">
+                  <i class="fa-solid fa-trash"></i>
+                </button>
+              </div>
+
+              <div class="button_container">
+                <button class="button"><i class="fa-solid fa-plus"></i></button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="line">
+      </div>`
+      ;
+
+    return textoHTML
+  }
 }
