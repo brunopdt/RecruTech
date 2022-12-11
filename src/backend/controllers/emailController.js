0acd923e8b090539a8da136fb16a0aa6b3cdbc00
Samuel Leal <samuelmarquessousaleal@gmail.com>
2022-12-11 19:32:55 -0300

    email para revisao #71

--- src/backend/controllers/emailController.js
+++ src/backend/controllers/emailController.js
@@ -3,15 +3,15 @@ const usuariosModel = require ('../models/usuariosModel');
 
 const enviarEmailController = async (req, res) => {
   const codigoUsuario = req.query.codUser;
-  const [usuario] = await usuariosModel.pegarUsuarioModel(codigoUsuario);
+  const usuario = await usuariosModel.pegarUsuarioModel(codigoUsuario);
   const sendEmail = await emailModel.run(usuario);
   return res.status(200).json(sendEmail);
 };
 
 const enviarEmailNegativoController = async (req, res) => {
-  console.log(codigoUsuario)
   const codigoUsuario = req.query.codUser;
-  const [usuario] = await usuariosModel.pegarUsuarioModel(codigoUsuario);
+  const usuario = await usuariosModel.pegarUsuarioModel(codigoUsuario);
+  console.log(usuario)
   const sendEmail = await emailModel.runNegativo(usuario);
   return res.status(200).json(sendEmail);
 }; 
