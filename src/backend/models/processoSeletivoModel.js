6059426ad8e75a0542d5d30426b6421850758bb4
Barbara Mattioly Andrade <bmattiolyandrade@gmail.com>
2022-12-14 03:34:34 -0300

    Ajustado status curriculo ao aplicar filtro #86

--- src/backend/models/processoSeletivoModel.js
+++ src/backend/models/processoSeletivoModel.js
@@ -14,7 +14,7 @@ const listarCurriculosModel = async (codigoVaga) => {
 
 const listarCurriculosFiltradosModel = async (codigoVaga) => {
     const [curriculos] = await connection.execute(`
-        SELECT u.nome, cv.codigoCandidato, cv.curriculo, v.tituloVaga, v.tempoExperienciaVaga, cv.tempoExperiencia FROM usuario u
+        SELECT u.nome, cv.codigoCandidato, cv.curriculo, v.tituloVaga, v.tempoExperienciaVaga, cv.tempoExperiencia, cv.indCurriculoAprovado FROM usuario u
             JOIN candidato_vaga cv
                 ON u.codigoUsuario = cv.codigoCandidato
             JOIN vaga v
