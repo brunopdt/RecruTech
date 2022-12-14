af33015876764d7d497508df08ec97cde4e2d7e0
Barbara Mattioly Andrade <bmattiolyandrade@gmail.com>
2022-12-14 10:38:42 -0300

    Rotas #92

--- src/api/app.js
+++ src/api/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const router = require('./router');
+const router = require('../backend/router');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+    
 const app = express();
 
 app.use(express.json());
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const app = require('./backend/app');
+const app = require('./api/app');
 const port = 8081;
-
+console.log("Entrou aqui")
 app.listen(port, function () {
     console.log("Servidor rodando na porta 8081 (http://localhost:8081)")
 })
