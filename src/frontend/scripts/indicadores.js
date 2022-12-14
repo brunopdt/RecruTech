71b9b0c85d16963642705fa09eb41a4766202a77
Bruna <bruna.portilho24@gmail.com>
2022-12-14 01:25:58 -0300

    Integração inidcador 1 #91

--- src/frontend/scripts/indicadores.js
+++ src/frontend/scripts/indicadores.js
@@ -1,5 +1,6 @@
 const uri = "http://localhost:8081/indicadores"
 const taxaMesUri = "/indicador-taxa-vagas"
+const taxaUsuariosUri = "/indicador-taxa-usuarios"
 
 const abrirIndicador = async (codigo) => {
   window.location.href = `/indicadores`;
@@ -23,6 +24,26 @@ async function getVagas() {
     })
 
     taxaCrescimentoVaga(data);
+
+};
+const taxaCrescimentoUsuario = (data) => {
+  const pTaxa = document.getElementById("novosUsuarios");
+  pTaxa.innerHTML = data;
+};
+
+async function getUsuarios() {
+  const headers = new Headers()
+  headers.append('Content-Type', 'application/json')
+  headers.append('Accept', 'application/json')
+
+  const data = await fetch(taxaUsuariosUri, {
+    method: 'GET',
+    headers: headers
+  }).then(response => {
+    return response.json()
+  })
+
+  taxaCrescimentoVaga(data);
 
 };
 
